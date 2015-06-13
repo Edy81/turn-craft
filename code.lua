e0594277d6ab89bf1426e319b109b019ec323896
Edy81 <quad95@msn.com>
2015-06-13 19:17:50 +0200

    changed id codes for blocks and added new ones

--- code.lua
+++ code.lua
@@ -3,38 +3,48 @@ function sandbox()
    0 = water
    1 = grass
    2 = solid block
-   3 = player can enter from south
-   4 = player can enter from north
-   5 = player can enter from east
-   6 = player can enter from west  
-   7 = player can enter for any direction
-   8 = npc
+   3 = north door
+   4 = south door
+   5 = east door
+   6 = west door  
+   7 = doors n and s
+   8 = doors n and e
+   9 = doors n and w
+  10 = doors s and e
+  11 = doors s and w
+  12 = doors w and e
+  13 = all 4 doors
+  14 = no door or ceiling
+  15 = npc inside
+  16 = npc out
+  
   11 = sticks object on a field
   12 = stones object on a field
   13 = leaves objec
   t on a field
-  14 =
+  20 =npc
+  40 = mirror inside block
   ]]--  
   i = 0
   repeat
     map[i] = 1 
     i = i + 1
-  until i == world.size 
+  until i==world.size 
   
   i = 0
   repeat
-    if i >= 0 and i <= 9 or i >= 91 and i <= 99 or i % 10 == 0 or i / 9 == 0 then
+    if i>=0 and i<=9 or i>=91 and i<=99 or i % 10==0 or i / 9==0 then
       map[i] = 0
-   -- elseif i % 10 == 0 then
+   -- elseif i % 10==0 then
       --map[i] = 0
-    elseif i % 9 == 9 then
+    elseif i % 9==9 then
       map[i] = 0
     elseif i % 10 ~= 0 then
  --     map[i] = 3
     
   end
   i = i +1
-  until i == world.size
+  until i==world.size
 
   
   if g.state== 1 then
@@ -42,22 +52,22 @@ function sandbox()
   repeat
     objects.c[i] = 0
     i = i+1
-  until i == world.size
+  until i==world.size
   
   i = 0
   repeat
-    if i >= 0 and i <= 9 or i >= 91 and i <= 99 or i % 10 == 0 or i / 9 == 0 then
+    if i>=0 and i<=9 or i>=91 and i<=99 or i % 10==0 or i / 9==0 then
       map[i] = 0
-   -- elseif i % 10 == 0 then
+   -- elseif i % 10==0 then
       --map[i] = 0
-    elseif i % 9 == 9 then
+    elseif i % 9==9 then
       map[i] = 0
     elseif i % 10 ~= 0 then
       map[i] = 3
     
     end
   i = i +1
-  until i == world.size
+  until i==world.size
 
   map[23] = 8
  -- map[35] = 3  
@@ -76,14 +86,14 @@ function sandbox()
       i =lm.random(world.size) 
     end
     i = i + 1
-  until i == world.size
+  until i==world.size
 end
 
 function place_object_on_grid(o,r)
     -- how many sticks and how many places have them.
   --t -- t for times that the loop must loop until 
   --local S = p.start
-  --if r == nil then
+  --if r==nil then
     local I = lm.random(11,100)
   --end
   local C
@@ -91,13 +101,13 @@ function place_object_on_grid(o,r)
   -- the sticks will be place on fields randonly
   repeat
    local n = lm.random(1,10) -- n for number of objects   
-    if map[I] ==  1 then
+    if map[I]== 1 then
       map[I] = o
-      if o == 11 then
+      if o==11 then
        local name = 'sticks'
-      elseif o == 12 then
+      elseif o==12 then
         local name = 'stones'
-      elseif o == 14 then
+      elseif o==14 then
         local name = 'grass'
       end
       objects.id[I] = o
@@ -111,15 +121,15 @@ function place_object_on_grid(o,r)
       I = I+ 1
     end
     
-  until C == 10
+  until C==10
 end
 
 function game_rules_default()
   -- day cycle rules
-  if p.turn_p == p.turn_c -1 then
+  if p.turn_p==p.turn_c -1 then
     time = time + 1
   end
-  if time % 1 == 0 then
+  if time % 1==0 then
     g.day_cycle = g.day_cycle + 1
    -- time = 0
   end
