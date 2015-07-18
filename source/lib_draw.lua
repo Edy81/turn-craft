05b7ebf66beb6b8f35c56e642110c3597d98e6e4
Edy81 <quad95@msn.com>
2015-07-18 16:23:42 +0200

    Added npc, fixed a bit blocks

--- source/lib_draw.lua
+++ source/lib_draw.lua
@@ -1,5 +1,4 @@
-
-function draw_left_side_wall_in()
+function draw_l_side_wall_in()
   -- left side wall
   -- /
   lg.setColor(C_BLACK)
@@ -9,7 +8,7 @@ lg.polygon('fill',w/3,h/2+96 , w/2-64,h/2+56, w/2-64,h/2-24, w/3,h/2-96 )
   --lg.polygon('line',w/3,h/2+96 , w/2-48,h/2+56, w/2-48,h/2-24, w/3,h/2-96)
 end
 
-function draw_left_side_wall_in_2()
+function draw_l_side_wall_in_2()
   -- left side wall
   -- /
   lg.setColor(C_BLACK)
@@ -19,7 +18,7 @@ lg.polygon('fill',w/3,h/2+48 , w/2-64,h/2+38, w/2-64,h/2-12, w/3,h/2-48 )
   --lg.polygon('line',w/3,h/2+96 , w/2-48,h/2+56, w/2-48,h/2-24, w/3,h/2-96)
 end
 
-function draw_left_side_wall()
+function draw_l_side_wall()
   --left wall visible inside a room or if a building is next to it
   lg.setColor(C_BLACK)
   lg.polygon('line',0,0 , 0,h, w/3,h/2+96, w/3,h/2-96 )
@@ -27,18 +26,23 @@ function draw_left_side_wall()
   lg.polygon('fill',0,0 , 0,h, w/3,h/2+96, w/3,h/2-96 )
 end
 
-function draw_front_left_wall()
+function draw_front_l_wall()
   lg.setColor(C_BLACK)
   lg.rectangle('line',1,h/2-96,w/3,192)
   lg.setColor(C_WHITE)
   lg.rectangle('fill',1,h/2-96,w/3,192)
 end
 
-function draw_front_left_wall_in_2()
+function draw_front_l_wall_in_2()
   lg.setColor(C_BLACK)
-  lg.rectangle('line',1,h/2-48,w/3,98)
+  lg.rectangle('line',w/6,h/2-48,w/6,98)
   lg.setColor(C_WHITE)
-  lg.rectangle('fill',1,h/2-48,w/3,98)
+  lg.rectangle('fill',w/6,h/2-48,w/6,98)
+end
+
+function draw_front_l_door_in_2()
+  lg.setColor(C_BLACK)
+  lg.rectangle('line',w/5,h/2-12, w/16,h/12)
 end
 
 function draw_front_wall()
@@ -51,37 +55,37 @@ function draw_front_wall()
     w/3,h/3)--224,192)
 end
 
-function draw_right_side_wall_in()
+function draw_r_side_wall_in()
   lg.setColor(C_BLACK)
   lg.polygon('line',w-w/3,h/2-96 , w/2+48,h/2-24, w/2+48,h/2+56, w-w/3,h/2+96 )
   lg.setColor(C_WHITE)
   lg.polygon('fill',w-w/3,h/2-96 , w/2+48,h/2-24, w/2+48,h/2+56, w-w/3,h/2+96 )
 end
 
-function draw_right_side_wall_in_2()
+function draw_r_side_wall_in_2()
   lg.setColor(C_BLACK)
-  lg.polygon('fill',w-w/3,h/2-48 , w/2+48,h/2-12, w/2+48,h/2+32,
+  lg.polygon('line',w-w/3,h/2-48 , w/2+96,h/2-12, w/2+96,h/2+32,
     w-w/3,h/2+48 )
   lg.setColor(C_WHITE)
-  lg.polygon('fill',w-w/3,h/2-48 , w/2+48,h/2-12, w/2+48,h/2+32,
+  lg.polygon('fill',w-w/3,h/2-48 , w/2+96,h/2-12, w/2+96,h/2+32,
     w-w/3,h/2+48 )
 end
+
+function draw_front_r_wall_in_2()
+  lg.setColor(C_BLACK)
+  lg.rectangle('line',w-w/3,h/2-48,w/6,98)  
+  lg.setColor(C_WHITE)
+  lg.rectangle('fill',w-w/3,h/2-48,w/6,98)
+end
   
-function draw_front_right_wall()
+function draw_front_r_wall()
   lg.setColor(C_BLACK)
   lg.rectangle('line',w-w/3,h/2-96,w/3,192)  
   lg.setColor(C_WHITE)
   lg.rectangle('fill',w-w/3,h/2-96,w/3,192)
 end
 
-function draw_front_right_wall_in_2()
-  lg.setColor(C_BLACK)
-  lg.rectangle('line',w-w/3,h/2-48,w/3,98)  
-  lg.setColor(C_WHITE)
-  lg.rectangle('fill',w-w/3,h/2-48,w/3,98)
-end
-
-function draw_right_side_wall()
+function draw_r_side_wall()
   --right side wall
   lg.setColor(C_BLACK)
   lg.polygon('line',w,h , w,0, w-w/3,h/2-96, w-w/3,h/2+96 )  
@@ -91,8 +95,35 @@ end
 
 function draw_door()
   lg.setColor(C_BLACK)
-  lg.rectangle('line',w/2-32,h/2-32,64,128)
+  lg.rectangle('line',w/2-48,h/2-24,112,128)
 end          
+
+function draw_door_l_wall()
+  lg.setColor(C_BLACK)
+  lg.polygon('line',w/2-192-64,h/2-32, w/2-256-128,h/2-64,
+    w/2-256-128,h/2+116+108, w/2-192-64, h/2+148)
+end
+
+function draw_door_r_wall()
+  lg.setColor(C_BLACK)
+  lg.polygon('line',w/2+192+64,h/2-32, w/2+256+128,h/2-64,
+  w/2+256+128,h/2+116+108, w/2+192+64, h/2+148)
+end
+
+function draw_front_l_side_door()
+  lg.setColor(C_BLACK)
+  lg.rectangle('line',w/6,h/2-12, w/16,h/6)
+end
+
+function draw_front_r_side_door()
+  lg.setColor(C_BLACK)
+  lg.rectangle('line',w-w/6,h/2-12, w/16,h/6)
+end
+
+function draw_front_r_door_in_2()
+  lg.setColor(C_BLACK)
+  lg.rectangle('line',w-w/3,h/2-24, w/16,h/8)
+end
 
 function draw_distance_2_block()
   lg.setColor(C_BLACK)
@@ -168,7 +199,7 @@ function draw_table()
   w-w/4,h/2+h/8)
 end
 
-function draw_table_right_side()
+function draw_table_r_side()
   lg.setColor(C_BLACK)
   lg.rectangle('line', w-64,h/2+h/4-64, 64, 64)
   lg.polygon('line',  w-64,h/2+h/8, w-64,h/2+h/4, w-w/4,h/2+h/8,
@@ -183,15 +214,25 @@ function draw_npc()
   -- lg.rectangle("fill",1,h/2,w,h/2)
   -- hair
   lg.setColor(C_BLACK)
-  lg.rectangle("fill",w/2-16,h/2-12,48,16)
+  lg.rectangle("fill",w/2-12,h/2-12,48,16)
   -- face
-  lg.setColor(C_GRAY)
-  lg.rectangle("fill",w/2-16,h/2+4,48,48)
-  -- body
-  lg.setColor(C_BLACK)
-  lg.rectangle('line',w/2-24,h/2+72,72,72)
+  lg.setColor(C_SKIN)
+  lg.rectangle("fill",w/2-12,h/2+4,48,48)
+  -- eyes
+  lg.setColor(C_BLACK)                   -- left eye
+  lg.rectangle('line',w/2-8,h/2+8,12,12)
   lg.setColor(C_WHITE)
-  lg.rectangle('fill',w/2-24,h/2+52,70,70)  
+  lg.rectangle('fill',w/2-8,h/2+8,12,12)  
+
+  lg.setColor(C_BLACK)                   -- right eye
+  lg.rectangle('line',w/2+16,h/2+8,12,12)
+  lg.setColor(C_WHITE)
+  lg.rectangle('fill',w/2+16,h/2+8,12,12) 
+  -- body
+  lg.setColor(C_YELLOW)
+  lg.rectangle('fill',w/2-24,h/2+52,70,70) 
+  lg.setColor(C_BLACK)
+  lg.rectangle('line',w/2-24,h/2+52,72,72)
   lg.setColor(C_SKIN)  
   lg.polygon('fill', w/2,h/2+52, w/2+24,h/2+52, w/2+12,h/2+72) 
     --w/2-22,h/2+52, w/2-22,h/2+52+70, w/2-22+70,h/2+52+70,
@@ -205,8 +246,18 @@ function draw_npc()
   lg.setColor(C_WHITE)
   lg.rectangle('fill',w/2-32,h/2+56,6,62)
   
+  lg.setColor(C_BLACK)
   lg.rectangle('line',w/2+48,h/2+56,8,64)  -- right arm
+  lg.setColor(C_WHITE)
+  lg.rectangle('fill',w/2+48,h/2+56,8,64)
   -- legs
+  lg.setColor(C_BLACK)
   lg.rectangle('line',w/2-16,h/2+126,24,64)  -- left leg
+  lg.setColor(C_BROWN)
+  lg.rectangle('fill',w/2-16,h/2+126,24,64) 
+  
+  lg.setColor(C_BLACK)
   lg.rectangle('line',w/2+16,h/2+126,24,64)  -- right leg
+  lg.setColor(C_BROWN)
+  lg.rectangle('fill',w/2+16,h/2+126,24,64) 
 end
